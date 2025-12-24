a6fdeea37068d7f30d44e7968f4d4052a0afdbcf
Syed Rizwan <syedbbictech@gmail.com>
2025-12-24 13:10:45 +0530

    Add files via upload

---
+++ Session_1.py
@@ -0,0 +1,71 @@
+"""name =input("Enter a name")
+print("Hello",name)"""
+
+"""number=input("Enter your number: ")
+print(type(number))"""
+
+"""number=int(input("Enter a number: "))
+print("You entered",type(number))"""
+
+#Valid variable names
+"""my_variable=10
+print(my_variable)
+value9=100
+print(value9)
+_value=200
+print(_value)"""
+
+# Invalid variable names
+"""9value=300
+name@= "John"
+and="Yes" """
+
+#Arthmetic Operators
+"""print(10+5)  #15
+
+a=20
+b=10
+print(a-b)  #10
+print(a*b)  #200
+print(a/b)  #2.0
+print(a%b)  #0
+print(a//b) #2
+print(a**b) #10240000000000"""
+
+#Comparison Operators
+"""a=10
+b=20
+print(a==b)  #False
+print(a!=b)  #True
+print(a>b)   #False
+print(a<b)   #True
+print(a>=b)  #False
+print(a<=b)  #True"""
+
+#Logical Operators
+"""a=True
+b=False
+print(a and b)  #False
+print(a or b)   #True
+print(not a)    #False
+print(not b)    #True"""
+
+#Assignment Operators
+"""x=10
+x+=5
+print(x)  #15
+x-=3
+print(x)  #12
+x*=2
+print(x)  #24
+x/=4
+print(x)  #6.0
+x%=4
+print(x)  #2.0
+x//=2
+print(x)  #1.0"""
+
+#Bitwise Operators
+a=10  
+b=6
+print(a & b)  #2
